fe5373a854a5c431103d69b1dacb205093b73553
kartik <kebijebi@gmail.com>
2021-08-09 02:05:28 +0100

    changed config.yml file

--- cypress/integration/Test/TC_01_BasicTests.spec.js
+++ cypress/integration/Test/TC_01_BasicTests.spec.js
@@ -1,5 +1,5 @@
 // <referen/ce types="Cypress" />
-import HomePage from '../../support/pageObjects/HomePage.js'
+import HomePage from '../../support/pageObjects/HomePage'
 
 const homePage = new HomePage()
 
